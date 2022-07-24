30245d4104d5523f6319b9fd093acb379750eeff
hanbeulYou <hanbeul.you@gmail.com>
2022-07-24 15:37:38 +0900

    220724 algorithm solved

---
+++ BAEKJOON/For_IM/2669.py
@@ -0,0 +1,14 @@
+plane = [[0 for _ in range(10)] for __ in range(10)]
+
+for _ in range(4) :
+    left_x, left_y, right_x, right_y = map(int, input().split())
+    
+    for y in range(left_y, right_y) :
+        for x in range(left_x, right_x) :
+            plane[y][x] = 1
+
+total_sum = 0
+for y in range(10) :
+    total_sum += sum(plane[y])
+    
+print(total_sum)
