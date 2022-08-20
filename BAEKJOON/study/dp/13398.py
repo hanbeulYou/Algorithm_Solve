6259897f2d59875415041c77ae52815a2d5eb649
Hanbeul You <109324631+hanbeulYou@users.noreply.github.com>
2022-08-21 01:38:02 +0900

    220821 algorithm solved

--- BAEKJOON/study/dp/13398.py
+++ BAEKJOON/study/dp/13398.py
@@ -3,22 +3,18 @@ arr = list(map(int, input().split()))
 dp = []
 without_dp = []
 
-if arr[0] < 0 :
-    dp.append(0)
-else :
-    dp.append(arr[0])
+# 한 번도 제외시키지 않고 만들 수 있는 최적의 해
+dp.append(arr[0])
+# 한 번 제외시키고 만들 수 있는 최적의 해
 without_dp.append(dp[0])
     
 for idx in range(1, N) :
-    # 새로운 값이 더해지는게 최선이 아닐 경우
-    if arr[idx] + dp[idx-1] < dp[idx-1] :
-        dp.append(max(0, arr[idx] + dp[idx-1]))
-        # 새로운 값을 제외했을 때 최선의 값
-        without_dp.append(max(without_dp[idx-1], dp[idx-1]))
-        
-    else :
-        dp.append(dp[idx-1] + arr[idx])
-        without_dp.append(dp[idx-1] + arr[idx])
-
-print(dp)
-print(without_dp)
+    # 현재 값을 포함해 제외 없이 만들 수 있는 최선의 해 
+    # 한번도 제외 안한 애들의 최선의 해 + 현재 값 vs 현재 값
+    dp.append(max(dp[idx-1] + arr[idx], arr[idx]))
+    
+    # 현재 값을 포함해 한 번의 제외로 만들 수 있는 최선의 해 
+    # 한 번도 제외 안했을 때 현재 값 제외 vs 앞의 것들에서 한 번이라도 제외했을 때 최선의 해 + 현재 값
+    without_dp.append(max(dp[idx-1], without_dp[idx-1]+arr[idx]))
+    
+print(max(map(max, dp, without_dp)))
