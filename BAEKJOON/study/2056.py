7adc7978f9b26f3020bbc870223e17b0bb44f200
hanbeulYou <hanbeul.you@gmail.com>
2022-09-19 23:21:43 +0900

    220919 algorithm solved

--- BAEKJOON/study/2056.py
+++ BAEKJOON/study/2056.py
@@ -1,8 +1,13 @@
 N = int(input())
 graph = [[] for _ in range(N+1)]
 times = [0 for _ in range(N+1)]
-for i in range(N):
+for i in range(1, N+1):
     input_arr = list(map(int, input().split()))
-    times[i+1]
-    if input_arr[i][1]:
-        for j in range(input_arr[i][1]):
+    times[i] = input_arr[0]
+    if input_arr[1]:
+        max_time = 0
+        for j in input_arr[2:]:
+            max_time = max(max_time, times[j])
+        times[i] += max_time
+
+print(max(times))
