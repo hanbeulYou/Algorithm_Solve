27c9ff26dda7ca9ef0e34f5c6971a9e0e31fe975
hanbeulYou <hanbeul.you@gmail.com>
2022-09-19 23:10:33 +0900

    220919 algorithm solved

---
+++ BAEKJOON/study/2056.py
@@ -0,0 +1,8 @@
+N = int(input())
+graph = [[] for _ in range(N+1)]
+times = [0 for _ in range(N+1)]
+for i in range(N):
+    input_arr = list(map(int, input().split()))
+    times[i+1]
+    if input_arr[i][1]:
+        for j in range(input_arr[i][1]):
